588a7b2b19f81972854a0fb762b63d64ff81fb2f
crashdemons <crashenator+gh@gmail.com>
2018-12-26 06:13:17 -0600

    toolbox: isolate event methods to disallow "this" (which would fail silently before)

--- js/toolbox.js
+++ js/toolbox.js
@@ -51,20 +51,6 @@ if (typeof jxlate.ui === "undefined") {
                 this.addtooln(ts[i]);
         },
 
-        show: function () {
-            this.tbox.style.display = 'block';
-        },
-        hide: function () {
-            this.tbox.style.display = 'none';
-        },
-        toggle: function () {
-            if (this.shown)
-                this.hide();
-            else
-                this.show();
-            this.shown = !this.shown;
-        },
-
         setpos: function (x, y) {
             this.tbox.style.left = x + "px";
             this.tbox.style.top = y + "px";
@@ -79,7 +65,7 @@ if (typeof jxlate.ui === "undefined") {
             this.addtool(this.tools[i][0], this.tools[i][1]);
         },
         addtool: function (sname, sdesc) {
-            var sfunc = "jxlate.ui.toolbox.action_" + sname;
+            var sfunc = "jxlate.ui.toolbox.events.action_" + sname;
             
             var tool = document.createElement("div");
             tool.className = "tool";
@@ -97,68 +83,85 @@ if (typeof jxlate.ui === "undefined") {
 
         },
 
-        action_debug: function () {
-            alert("wat");
-        },
+        events:{
+            
+            show: function () {
+                jxlate.ui.toolbox.tbox.style.display = 'block';
+            },
+            hide: function () {
+                jxlate.ui.toolbox.tbox.style.display = 'none';
+            },
+            toggle: function () {
+                if (jxlate.ui.toolbox.shown)
+                    jxlate.ui.toolbox.events.hide();
+                else
+                    jxlate.ui.toolbox.events.show();
+                jxlate.ui.toolbox.shown = !jxlate.ui.toolbox.shown;
+            },
+            
+            action_debug: function () {
+                alert("wat");
+            },
 
-        action_replace: function () {
-            var search = prompt("Enter the string to replace", "");
-            if (search === null)
-                return;
-            var replace = prompt("Enter the string to replace with", "");
-            if (replace === null)
-                return;
-            this.textarea.value = jxlate.formatter.replaceAll(search, replace, this.textarea.value);
-        },
-        action_case: function () {
-            if (this.lettercase)
-                this.textarea.value = this.textarea.value.toUpperCase();
-            else
-                this.textarea.value = this.textarea.value.toLowerCase();
-            this.lettercase = !this.lettercase;
-        },
-        action_length: function () {
-            var l = this.textarea.value.length;
-            var sl = this.strippedlen(this.textarea.value);
-            var out = "Total length: " + l;
-            var base = jxlate.translator.mode_bases[jxlate.ui.mode];
-            if (base === 2) {
-                out += "\nBit length: " + sl;
-                out += "\nBytes: " + (sl / 8);
-            } else if (base === 16) {
-                out += "\nDigits: " + sl + " (nibbles)";
-                out += "\nBytes: " + (sl / 2);
+            action_replace: function () {
+                var search = prompt("Enter the string to replace", "");
+                if (search === null)
+                    return;
+                var replace = prompt("Enter the string to replace with", "");
+                if (replace === null)
+                    return;
+                this.textarea.value = jxlate.formatter.replaceAll(search, replace, this.textarea.value);
+            },
+            action_case: function () {
+                if (this.lettercase)
+                    this.textarea.value = this.textarea.value.toUpperCase();
+                else
+                    this.textarea.value = this.textarea.value.toLowerCase();
+                this.lettercase = !this.lettercase;
+            },
+            action_length: function () {
+                var l = this.textarea.value.length;
+                var sl = this.strippedlen(this.textarea.value);
+                var out = "Total length: " + l;
+                var base = jxlate.translator.mode_bases[jxlate.ui.mode];
+                if (base === 2) {
+                    out += "\nBit length: " + sl;
+                    out += "\nBytes: " + (sl / 8);
+                } else if (base === 16) {
+                    out += "\nDigits: " + sl + " (nibbles)";
+                    out += "\nBytes: " + (sl / 2);
+                }
+                alert(out);
+            },
+            action_reverse: function () {
+                this.textarea.value = this.textarea.value.reverse();
+            },
+            action_shift: function () {
+                var shift = prompt("Please enter the caesar shift value", "13");
+                if (shift === null)
+                    return;
+                shift = parseInt(shift);
+                if (shift === 0)
+                    return;
+                this.textarea.value = Caesar(1, this.textarea.value, shift);
+            },
+
+            action_invert: function () {//NOTE: uses 'mode' and 'mode_bases' global from ui.js
+                var base = jxlate.ui.mode_bases[jxlate.ui.mode];
+                var tmp = jxlate.ui.convertText(this.textarea.value, base, 2);
+                tmp = this.invertbits(tmp);
+                this.textarea.value = jxlate.ui.convertText(tmp, 2, base);
+            },
+            action_greverse: function () {
+                this.textarea.value = this.textarea.value.split("").reverse().join("").split(" ").reverse().join(" ");
+            },
+            action_stripspaces: function () {
+                //TODO: operate on existing value instead of re-reading
+                this.textarea.value = jxlate.formatter.replaceAll(" ", "", this.textarea.value);
+                this.textarea.value = jxlate.formatter.replaceAll("\t", "", this.textarea.value);
+                this.textarea.value = jxlate.formatter.replaceAll("\r", "", this.textarea.value);
+                this.textarea.value = jxlate.formatter.replaceAll("\n", "", this.textarea.value);
             }
-            alert(out);
-        },
-        action_reverse: function () {
-            this.textarea.value = this.textarea.value.reverse();
-        },
-        action_shift: function () {
-            var shift = prompt("Please enter the caesar shift value", "13");
-            if (shift === null)
-                return;
-            shift = parseInt(shift);
-            if (shift === 0)
-                return;
-            this.textarea.value = Caesar(1, this.textarea.value, shift);
-        },
-
-        action_invert: function () {//NOTE: uses 'mode' and 'mode_bases' global from ui.js
-            var base = jxlate.ui.mode_bases[jxlate.ui.mode];
-            var tmp = jxlate.ui.convertText(this.textarea.value, base, 2);
-            tmp = this.invertbits(tmp);
-            this.textarea.value = jxlate.ui.convertText(tmp, 2, base);
-        },
-        action_greverse: function () {
-            this.textarea.value = this.textarea.value.split("").reverse().join("").split(" ").reverse().join(" ");
-        },
-        action_stripspaces: function () {
-            //TODO: operate on existing value instead of re-reading
-            this.textarea.value = jxlate.formatter.replaceAll(" ", "", this.textarea.value);
-            this.textarea.value = jxlate.formatter.replaceAll("\t", "", this.textarea.value);
-            this.textarea.value = jxlate.formatter.replaceAll("\r", "", this.textarea.value);
-            this.textarea.value = jxlate.formatter.replaceAll("\n", "", this.textarea.value);
         },
 //==============================================
 
