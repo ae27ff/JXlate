7973506648c45566ceeba3729d5c9e3a2b5f4985
crashdemons <crashenator+gh@gmail.com>
2018-12-26 06:50:37 -0600

    correct reference

--- js/core.js
+++ js/core.js
@@ -9,6 +9,6 @@ var jxlate = {//we're not really going to polymorphism here, so we can do with a
         this.util.init();
         this.formatter.init();
         this.translator.init();
-        if(initializeUI) this.ui.init();
+        if(this.initializeUI) this.ui.init();
     }
 };
